1f83d9c16f7172d9a7c88a2432be12e1feb98079
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-21 15:05:38 +0200

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteFile();
+        }
+
+        static public  void DeleteFile()
+        {
+            string FileDir = @"/Users/tyr1k_qq/Desktop";
+            DirectoryInfo DirInf = new DirectoryInfo(FileDir );
+            try
+            {
+                if (DirInf.Exists)
+                {
+                    DateTime LastTime = DirInf.LastAccessTime;
+                    
+                    if (( DateTime.Now - LastTime) > TimeSpan.FromMinutes(30)) 
+                    {
+                        DirInf.Delete(true);
+                        Console.WriteLine("Your file is Delete");
+                    }
+                    else
+                    {
+                        Console.WriteLine("File used za poslednie 30 minut");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Wrong adress");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
